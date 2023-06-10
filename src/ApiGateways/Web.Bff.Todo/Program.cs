4097e2666231f66ae2dfa58855162bddc96e978c
zcqiand <zcqiand@163.com>
2023-06-10 20:31:59 +0800

    网关授权内容改成配置

--- src/ApiGateways/Web.Bff.Todo/Program.cs
+++ src/ApiGateways/Web.Bff.Todo/Program.cs
@@ -2,15 +2,22 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Todo.API.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+var bffTodoConfiguration = configuration.GetSection(nameof(BffTodoConfiguration)).Get<BffTodoConfiguration>();
+services.AddSingleton(bffTodoConfiguration);
 
 RegisterJsonFile(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-RegisterAuthentication(builder.Services);
+RegisterAuthentication(builder.Services, BffTodoConfiguration bffTodoConfiguration);
 
 builder.Services.AddOcelot();
 
@@ -28,14 +35,14 @@ app.UseOcelot().Wait();
 app.Run();
 
 // ÑéÖ¤
-static void RegisterAuthentication(IServiceCollection services)
+static void RegisterAuthentication(IServiceCollection services, BffTodoConfiguration bffTodoConfiguration)
 {
     var authenticationProviderKey = "OcelotKey";
     services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(authenticationProviderKey, options =>
                 {
-                    options.Authority = "https://localhost:44310";
-                    options.RequireHttpsMetadata = true;
+                    options.Authority = bffTodoConfiguration.IdentityServerBaseUrl;
+                    options.RequireHttpsMetadata = bffTodoConfiguration.RequireHttpsMetadata;
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateAudience = false
